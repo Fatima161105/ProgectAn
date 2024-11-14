cc585d9f1d0a698c52f53e21237e49c9816da281
Fatima161105 <fotima.yakubova2005@gmail.com>
2024-11-14 13:50:48 +0500

    Add parser

--- src/models/Assignment.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class Assignment {
-    private String type; // Тип задания (УПР, ДЗ, КВ)
-    private String name; // Название задания
-
-    // Конструктор, геттеры, сеттеры
-    // ...
-}
--- src/models/Grade.java
+++ src/models/Grade.java
@@ -3,9 +3,26 @@ package models;
 public class Grade {
     private Student student;
     private Topic topic;
-    private Assignment assignment;
     private int grade;
 
-    // Конструктор, геттеры, сеттеры
-    // ...
+    // Конструктор
+    public Grade(Student student, Topic topic, int grade) {
+        this.student = student;
+        this.topic = topic;
+        this.grade = grade;
+    }
+
+    // Геттеры
+    public Student getStudent() {
+        return student;
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+
+    public int getGrade() {
+        return grade;
+    }
 }
